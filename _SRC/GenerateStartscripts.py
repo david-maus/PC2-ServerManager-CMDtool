1c9db4cef8932a33b631d243507cbb12964b0024
David <info@david-maus.de>
2018-03-13 00:46:59 +0100

    Linux fix on startscriptmaker

--- _SRC/GenerateStartscripts.py
+++ _SRC/GenerateStartscripts.py
@@ -15,69 +15,123 @@ WARNING MESSY CODE! :)
 import glob
 import os
 import sys
+import pathlib
+import stat
 
-
-def replaceAll(folderCurrent):
+def replaceAll(folderCurrent, osPref):
     """Start the main function."""
     configFolder = os.path.abspath(os.path.join(folderCurrent,
                                                 '../', '../', 'configs'))
     scriptsFolder = os.path.abspath(os.path.join(folderCurrent,
                                                  'startscripts'))
-
+    pathlib.Path(scriptsFolder).mkdir(parents=False, exist_ok=True)
     os.chdir(scriptsFolder)
-    for file in glob.glob("*.bat"):
-        os.remove(file)
 
-    os.chdir(configFolder)
-    for file in glob.glob("*.ini"):
-        if 'basic.ini' in file:
-            pass
-        else:
-            scriptName = file.replace('.ini', '')
-            scriptFile = os.path.abspath(os.path.join(scriptsFolder,
-                                         scriptName + '.bat'))
-            scriptFilePR = os.path.abspath(os.path.join(scriptsFolder,
-                                           scriptName + '-Practice.bat'))
-            scriptFileSun = os.path.abspath(os.path.join(scriptsFolder,
-                                            scriptName + '-Sun.bat'))
-            scriptFilePR_Sun = os.path.abspath(os.path.join(scriptsFolder,
-                                               scriptName +
-                                               '-PracticeSun.bat'))
+    if(osPref == "win"):
+        for file in glob.glob("*.bat"):
+            os.remove(file)
 
-            scriptWriter = open(scriptFile, "w")
-            scriptWriter.write('cd ../\n')
-            scriptWriter.write('DedicatedServerWrapper.exe ' + file)
-            scriptWriter.close()
+        os.chdir(configFolder)
+        for file in glob.glob("*.ini"):
+            if 'basic.ini' in file:
+                pass
+            else:
+                scriptName = file.replace('.ini', '')
+                scriptFile = os.path.abspath(os.path.join(scriptsFolder,
+                                             scriptName + '.bat'))
+                scriptFilePR = os.path.abspath(os.path.join(scriptsFolder,
+                                               scriptName + '-Practice.bat'))
+                scriptFileSun = os.path.abspath(os.path.join(scriptsFolder,
+                                                scriptName + '-Sun.bat'))
+                scriptFilePR_Sun = os.path.abspath(os.path.join(scriptsFolder,
+                                                   scriptName +
+                                                   '-PracticeSun.bat'))
 
-            scriptWriter = open(scriptFilePR, "w")
-            scriptWriter.write('cd ../\n')
-            scriptWriter.write('DedicatedServerWrapper.exe ' + file +
-                               ' PracticeServer')
-            scriptWriter.close()
+                scriptWriter = open(scriptFile, "w")
+                scriptWriter.write('cd ../\n')
+                scriptWriter.write('DedicatedServerWrapper.exe ' + file)
+                scriptWriter.close()
 
-            scriptWriter = open(scriptFileSun, "w")
-            scriptWriter.write('cd ../\n')
-            scriptWriter.write('DedicatedServerWrapper.exe ' + file +
-                               ' Sunshine')
-            scriptWriter.close()
+                scriptWriter = open(scriptFilePR, "w")
+                scriptWriter.write('cd ../\n')
+                scriptWriter.write('DedicatedServerWrapper.exe ' + file +
+                                   ' PracticeServer')
+                scriptWriter.close()
 
-            scriptWriter = open(scriptFilePR_Sun, "w")
-            scriptWriter.write('cd ../\n')
-            scriptWriter.write('DedicatedServerWrapper.exe ' + file +
-                               ' PracticeServer:Sunshine')
-            scriptWriter.close()
+                scriptWriter = open(scriptFileSun, "w")
+                scriptWriter.write('cd ../\n')
+                scriptWriter.write('DedicatedServerWrapper.exe ' + file +
+                                   ' Sunshine')
+                scriptWriter.close()
+
+                scriptWriter = open(scriptFilePR_Sun, "w")
+                scriptWriter.write('cd ../\n')
+                scriptWriter.write('DedicatedServerWrapper.exe ' + file +
+                                   ' PracticeServer:Sunshine')
+                scriptWriter.close()
+    else:
+        for file in glob.glob("*.sh"):
+            os.remove(file)
+
+        os.chdir(configFolder)
+
+        for file in glob.glob("*.ini"):
+            if 'basic.ini' in file:
+                pass
+            else:
+                scriptName = file.replace('.ini', '')
+                scriptFile = os.path.abspath(os.path.join(scriptsFolder,
+                                             scriptName + '.sh'))
+                scriptFilePR = os.path.abspath(os.path.join(scriptsFolder,
+                                               scriptName + '-Practice.sh'))
+                scriptFileSun = os.path.abspath(os.path.join(scriptsFolder,
+                                                scriptName + '-Sun.sh'))
+                scriptFilePR_Sun = os.path.abspath(os.path.join(scriptsFolder,
+                                                   scriptName +
+                                                   '-PracticeSun.sh'))
+
+                scriptWriter = open(scriptFile, "w")
+                scriptWriter.write('cd ../\n')
+                scriptWriter.write('DedicatedServerWrapper ' + file)
+                scriptWriter.close()
+                os.chmod(scriptFile, stat.S_IXGRP)
+
+                scriptWriter = open(scriptFilePR, "w")
+                scriptWriter.write('cd ../\n')
+                scriptWriter.write('DedicatedServerWrapper ' + file +
+                                   ' PracticeServer')
+                scriptWriter.close()
+                os.chmod(scriptFilePR, stat.S_IXGRP)
+
+                scriptWriter = open(scriptFileSun, "w")
+                scriptWriter.write('cd ../\n')
+                scriptWriter.write('DedicatedServerWrapper ' + file +
+                                   ' Sunshine')
+                scriptWriter.close()
+                os.chmod(scriptFileSun, stat.S_IXGRP)
+
+                scriptWriter = open(scriptFilePR_Sun, "w")
+                scriptWriter.write('cd ../\n')
+                scriptWriter.write('DedicatedServerWrapper ' + file +
+                                   ' PracticeServer:Sunshine')
+                scriptWriter.close()
+                os.chmod(scriptFilePR_Sun, stat.S_IXGRP)
 
 
 def main():
     """Start the main function."""
+    if sys.platform == "linux" or sys.platform == "linux2":
+        osPref = "lnx"
+    elif sys.platform == "win32":
+        osPref = "win"
     if getattr(sys, 'frozen', False):
         folderCurrent = os.path.dirname(sys.executable)
     else:
         folderCurrent = os.path.abspath(os.path.dirname(__file__))
         folderCurrent = os.path.abspath(os.path.join(folderCurrent,
-                                                     '../', 'bin', 'win'))
+                                                     '../', 'bin', osPref))
 
-    replaceAll(folderCurrent)
+    replaceAll(folderCurrent, osPref)
 
 
 if __name__ == "__main__":
