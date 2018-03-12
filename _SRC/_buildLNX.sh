d92f0b957f1fead0f5b1900e24e122e0ca25a70e
David <info@david-maus.de>
2018-03-12 17:15:11 +0100

    linux fix

--- _SRC/_buildLNX.sh
+++ _SRC/_buildLNX.sh
@@ -4,9 +4,42 @@ pyinstaller \
 --workpath ./__pyinstallerLNX__ \
 --specpath ./__specsLNX__ \
 --name "PC2_DedicatedServerWrapper" \
+--icon "icons/main.ico" \
 --upx-dir C:\_PORTABLE\PROGRAMS\UPX \
 --noupx \
 --noconsole \
 --noconfirm \
 --clean \
 PC2_DedicatedServerWrapper.py
+
+
+
+
+pyinstaller \
+-F \
+--distpath ../bin/lnx/ \
+--workpath ./__pyinstallerLNX__ \
+--specpath ./__specsLNX__ \
+--name "GenerateStartscripts" \
+--icon "icons/scripts.ico" \
+--upx-dir C:\_PORTABLE\PROGRAMS\UPX \
+--noupx \
+--noconsole \
+--noconfirm \
+--clean \
+make_startscripts.py
+
+
+pyinstaller \
+-F \
+--distpath ../bin/lnx/ \
+--workpath ./__pyinstallerLNX__ \
+--specpath ./__specsLNX__ \
+--name "GUI" \
+--icon "icons/main.ico" \
+--upx-dir C:\_PORTABLE\PROGRAMS\UPX \
+--noupx \
+--noconsole \
+--noconfirm \
+--clean \
+__specsLNX__/GUI.spec
