3c3e9121c601aaf74103e53a7484f6778bee47cd
David <info@david-maus.de>
2018-03-12 16:54:00 +0100

    buildscript linux

--- _SRC/_buildLNX.sh
+++ _SRC/_buildLNX.sh
@@ -1,12 +1,12 @@
-pyinstaller ^
--F ^
---distpath ../bin/lnx/ ^
---workpath ./__pyinstallerLNX__ ^
---specpath ./__specsLNX__ ^
---name "PC2_DedicatedServerWrapper" ^
---upx-dir C:\_PORTABLE\PROGRAMS\UPX ^
---noupx ^
---noconsole ^
---noconfirm ^
---clean ^
+pyinstaller \
+-F \
+--distpath ../bin/lnx/ \
+--workpath ./__pyinstallerLNX__ \
+--specpath ./__specsLNX__ \
+--name "PC2_DedicatedServerWrapper" \
+--upx-dir C:\_PORTABLE\PROGRAMS\UPX \
+--noupx \
+--noconsole \
+--noconfirm \
+--clean \
 PC2_DedicatedServerWrapper.py
